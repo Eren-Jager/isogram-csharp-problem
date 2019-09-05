812321804edc77445b4ac61790ff24ec26abb42e
Chandrakanth Ande <chandrakanth.ande@cgi.com>
2019-09-05 09:58:26 +0530

    finished

---
+++ isogram-csharp-problem/Isogram.Tests/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using isogram_csharp_problem;
+
+namespace Isogram.Tests
+{
+    public class Class1
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("isogram", true)]
+        [InlineData("eleven", false)]
+        [InlineData("Alphabet", true)]
+        [InlineData("subdermatoglyphic", true)]
+        [InlineData("thumbscrew-japingly", true)]
+        [InlineData("six-year-old", true)]
+        [InlineData("ofcial S s", true)]
+
+        public void Theory_Check(string value, bool expected)
+        {
+            Assert.Equal(expected, Program.checkiso(value));
+        }
+
+    }
+}
---
+++ isogram-csharp-problem/isogram-csharp-problem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace isogram_csharp_problem
+{
+    public class Program
+    {
+        public static bool checkiso(string s)
+        {
+            
+            s = s.Replace(" ", String.Empty);
+            s = s.Replace("-", String.Empty);
+            int i, j,k=0;
+            for (i = 0;i<s.Length-1;i++)
+            {
+                for(j=i+1;j<s.Length;j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        k = 1;
+                        break;
+                    }
+                }
+                if (k == 1) break;
+            }
+            if (k == 1) return false;
+            else return true;
+
+        }
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
